58b8c2603d76cb8bedb2491f05cbbf36580bd514
Nicolas Wazulek <nicolas.wazulek@niwa-dev.de>
2021-08-14 11:22:16 +0200

    feat(base): added Vector2D for positioning objects

---
+++ SnakeGame/Base/Vector2D.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace SnakeGame.Base
+{
+    public class Vector2D
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Vector2D(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public static Vector2D Zero
+        {
+            get
+            {
+                return new Vector2D(0, 0);
+            }
+        }
+    }
+}
