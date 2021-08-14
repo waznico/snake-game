f9d451c8982d4cee2416b89f6aac53eda69a8219
Nicolas Wazulek <nicolas.wazulek@niwa-dev.de>
2021-08-14 13:01:57 +0200

    feat(display): added method to add objects to render buffer

--- SnakeGame/Display/Renderer.cs
+++ SnakeGame/Display/Renderer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using SnakeGame.Base;
 
 namespace SnakeGame.Display
@@ -37,6 +38,45 @@ namespace SnakeGame.Display
                 {
                     RenderBuffer[y][x] = ' ';
                 }
+            }
+        }
+
+        /// <summary>
+        /// Adds an object with vector 2D positions to the render buffer. So in the next execution of rendering it'll be visible.
+        /// </summary>
+        /// <param name="positions">Positions to render given display elements</param>
+        /// <param name="displayElement">Character to render</param>
+        public void AddObjectToRenderer(IEnumerable<Vector2D> positions, char displayElement)
+        {
+            foreach (var position in positions)
+            {
+                if (IsPositionOverlappingDisplayBorders(position))
+                {
+                    ClearBuffer();
+                    throw new ArgumentException($"{nameof(ConsoleRenderer)}: Current position is overlapping with display borders. X: {position.X}; Y: {position.Y}");
+                }
+
+                RenderBuffer[position.Y][position.X] = displayElement;
+            }
+        }
+
+        /// <summary>
+        /// Check if a position is overlapping with display borders.
+        /// </summary>
+        /// <param name="currentPosition">Position to check</param>
+        /// <returns>Is overlapping = true; is within borders = false</returns>
+        private bool IsPositionOverlappingDisplayBorders(Vector2D currentPosition)
+        {
+            if (
+                (currentPosition.X > DisplaySize.X - 1 || currentPosition.Y > DisplaySize.Y - 1)
+                || (currentPosition.X < 0 || currentPosition.Y < 0)
+                )
+            {
+                return true;
+            }
+            else
+            {
+                return false;
             }
         }
 
